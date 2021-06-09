103075c0b7dfef54a7b1bf43397ebf476d071b85
Koramit Pichanaharee <koramit.p@gmail.com>
2021-06-09 20:46:02 +0700

    teaser

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <!-- <div>
         <form @submit.prevent="submit">
             <div>
                 <label for="login">login :</label>
@@ -12,6 +12,24 @@
             </div>
             <input type="submit" value="login">
         </form>
+    </div> -->
+    <div class="min-h-screen flex justify-center items-center bg-gradient-to-br from-blue-200 to-blue-800">
+        <div class="rounded-lg border-t-8 border-b-8 border-blue-600 shadow-md p-4 bg-white">
+            <form @submit.prevent="submit">
+            <div>
+                <label class="mt-2 font-semibold" for="login">login :</label>
+                <input class="form-input" type="text" name="login" id="login" v-model="form.login" required>
+                <p class="text-red-400 text-xs" v-if="form.errors.login">{{ form.errors.login }}</p>
+            </div>
+            <div>
+                <label class="mt-2 font-semibold" for="password">password :</label>
+                <input class="form-input" type="password" name="password" id="password" v-model="form.password" required>
+            </div>
+            <div class="float-right">
+                <button class="mt-2 p-2 rounded shadow bg-blue-400 hover:bg-blue-600 cursor-pointer text-white transition-colors duration-300 ease-in-out" type="submit">login</button>
+            </div>
+            </form>
+        </div>
     </div>
 </template>
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue({ version: 3 })
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require("tailwindcss"),
     ]);
